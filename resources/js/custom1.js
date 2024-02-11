9bc0f03a0ae9d95b35e1b0cf205e6abda9f7c24a
skybedy <skybedy@gmail.com>
2024-02-11 09:07:36 +0100

    110224hp

--- resources/js/custom1.js
+++ resources/js/custom1.js
@@ -120,13 +120,13 @@ $(document).on('click', 'a[href*="/event/result/"]', function(e) {
 
                 var value = response[key];
 
-                str += '<tr id="dynamic_result_individual_'+ key +'" class="bg-red-10">';
+                str += '<tr id="dynamic_result_individual_'+ key +'" class="bg-red-100 text-blue-700">';
 
                 str += '<td class="border" colspan="4"></td>';
 
                 str += '<td class="border px-2 text-center">'+response[key].date+'</td>'
 
-                str += '<td class="border text-center"><a class="result_map flex justify-center" href="/result/'+ resultId +'/map"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="red" class="w-6 h-6"><path fill-rule="evenodd" d="m7.539 14.841.003.003.002.002a.755.755 0 0 0 .912 0l.002-.002.003-.003.012-.009a5.57 5.57 0 0 0 .19-.153 15.588 15.588 0 0 0 2.046-2.082c1.101-1.362 2.291-3.342 2.291-5.597A5 5 0 0 0 3 7c0 2.255 1.19 4.235 2.292 5.597a15.591 15.591 0 0 0 2.046 2.082 8.916 8.916 0 0 0 .189.153l.012.01ZM8 8.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Z" clip-rule="evenodd" /></svg></a></td>';
+                str += '<td class="border text-center"><a class="result_map flex justify-center" href="/result/'+ resultId +'/map"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="blue" class="w-6 h-6"><path fill-rule="evenodd" d="m7.539 14.841.003.003.002.002a.755.755 0 0 0 .912 0l.002-.002.003-.003.012-.009a5.57 5.57 0 0 0 .19-.153 15.588 15.588 0 0 0 2.046-2.082c1.101-1.362 2.291-3.342 2.291-5.597A5 5 0 0 0 3 7c0 2.255 1.19 4.235 2.292 5.597a15.591 15.591 0 0 0 2.046 2.082 8.916 8.916 0 0 0 .189.153l.012.01ZM8 8.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Z" clip-rule="evenodd" /></svg></a></td>';
 
                 str += '<td class="border text-center">'+response[key].pace+'</td>';
 
