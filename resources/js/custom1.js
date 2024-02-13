932aa1e41c9f6a66c7bb703ab914016a911ab68f
skybedy <skybedy@gmail.com>
2024-02-13 22:01:05 +0000

    Upravy

--- resources/js/custom1.js
+++ resources/js/custom1.js
@@ -55,7 +55,7 @@ $(() => {
            if($(window).width() < 640)
            {
             //var novyRadek = $('<tr class="tr_map_xhr"><td class="text-center" colspan="5"><div id="m" style="height:300px"></div></td></tr>'); // Vytvoření nového řádku
-            var novyRadek = $('<tr class="tr_map_xhr '+ trId +'" style="position:relative"><td class="text-center" colspan="6"><div id="close_map" style="padding:2px 10px;border:2px solid black;background:white;position:absolute;right:100px;top:17px;z-index:1000;cursor:pointer">Zavřít mapu</div><div id="m" style="height:200px"></div></td></tr>'); // Vytvoření nového řádku
+            var novyRadek = $('<tr class="tr_map_xhr '+ trId +'" style="position:relative"><td class="text-center" colspan="6"><div id="close_map" style="padding:2px 10px;border:2px solid black;background:white;position:absolute;left:5px;top:5px;z-index:1000;cursor:pointer">Zavřít mapu</div><div id="m" style="height:200px"></div></td></tr>'); // Vytvoření nového řádku
 
             $("#result_table_sm #"+trId).after(novyRadek);
 
@@ -165,26 +165,46 @@ $(document).on('click', 'a[href*="/event/result/"]', function(e) {
 
                 str += '<tr id="dynamic_result_individual_'+ key +'" class="dynamic_result_individual_xhr user_'+ userId  +' bg-red-100 text-blue-700">';
 
-                str += '<td class="border" colspan="4"></td>';
+                if($(window).width() < 640)
+                {
+                    str += '<td class="border" colspan="2"></td>';
+                    
+                    str += '<td class="border px-2 text-center">'+response[key].date+'</td>'
 
-                str += '<td class="border px-2 text-center">'+response[key].date+'</td>'
+                    str += '<td class="border text-center"><a class="result_map result_map_xhr flex justify-center" href="/result/'+ response[key].id +'/map"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="blue" class="w-6 h-6"><path fill-rule="evenodd" d="m7.539 14.841.003.003.002.002a.755.755 0 0 0 .912 0l.002-.002.003-.003.012-.009a5.57 5.57 0 0 0 .19-.153 15.588 15.588 0 0 0 2.046-2.082c1.101-1.362 2.291-3.342 2.291-5.597A5 5 0 0 0 3 7c0 2.255 1.19 4.235 2.292 5.597a15.591 15.591 0 0 0 2.046 2.082 8.916 8.916 0 0 0 .189.153l.012.01ZM8 8.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Z" clip-rule="evenodd" /></svg></a></td>';
 
-                str += '<td class="border text-center"><a class="result_map result_map_xhr flex justify-center" href="/result/'+ response[key].id +'/map"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="blue" class="w-6 h-6"><path fill-rule="evenodd" d="m7.539 14.841.003.003.002.002a.755.755 0 0 0 .912 0l.002-.002.003-.003.012-.009a5.57 5.57 0 0 0 .19-.153 15.588 15.588 0 0 0 2.046-2.082c1.101-1.362 2.291-3.342 2.291-5.597A5 5 0 0 0 3 7c0 2.255 1.19 4.235 2.292 5.597a15.591 15.591 0 0 0 2.046 2.082 8.916 8.916 0 0 0 .189.153l.012.01ZM8 8.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Z" clip-rule="evenodd" /></svg></a></td>';
+                    str += '<td class="border text-center">'+response[key].finish_time+'</td>';
 
-                str += '<td class="border text-center">'+response[key].pace+'</td>';
+                    str += '<td class="border text-center">'+response[key].pace+'</td>';
+    
+                }
+                else
+                {
+                    str += '<td class="border" colspan="4"></td>';
+                    
+                    str += '<td class="border px-2 text-center">'+response[key].date+'</td>'
 
-                str += '<td class="border text-center">'+response[key].finish_time+'</td>';
+                    str += '<td class="border text-center"><a class="result_map result_map_xhr flex justify-center" href="/result/'+ response[key].id +'/map"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="blue" class="w-6 h-6"><path fill-rule="evenodd" d="m7.539 14.841.003.003.002.002a.755.755 0 0 0 .912 0l.002-.002.003-.003.012-.009a5.57 5.57 0 0 0 .19-.153 15.588 15.588 0 0 0 2.046-2.082c1.101-1.362 2.291-3.342 2.291-5.597A5 5 0 0 0 3 7c0 2.255 1.19 4.235 2.292 5.597a15.591 15.591 0 0 0 2.046 2.082 8.916 8.916 0 0 0 .189.153l.012.01ZM8 8.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Z" clip-rule="evenodd" /></svg></a></td>';
+    
+                    str += '<td class="border text-center">'+response[key].pace+'</td>';
+    
+                    str += '<td class="border text-center">'+response[key].finish_time+'</td>';
+    
+                    str += '<td class="border text-center"></td>';
+                }
 
-                str += '<td class="border text-center"></td>';
+               
+
+            
 
                 str += '</tr>';
 
-
-
-
-
             }
         }
+
+
+
+
 
         if($(window).width() < 640)
         {
--- resources/views/events/results/result-index.blade.php
+++ resources/views/events/results/result-index.blade.php
@@ -68,7 +68,13 @@
                                 @endif
                                 <tr class="odd:bg-gray-100 even:bg-white" id="result_{{ $result->id }}">
                                     <td class="border text-center">{{ $loop->iteration }}</td>
-                                    <td class="border px-2">{{ $result->lastname }} {{ $result->firstname }}</td>
+                                    <td class="border px-2">
+                                        @if($result->count > 1)
+                                            <a href="{{ route('result.user',$result->registration_id) }}" class="text-blue-700 underline">{{ $result->lastname }} {{ $result->firstname }}</a>
+                                        @else
+                                        {{ $result->lastname }} {{ $result->firstname }}
+                                        @endif
+                                    </td>
                                     <td class="border px-2">{{ $result->date }}</td>
                                     <td class="border px-2"><a class="result_map flex justify-center" href="{{ route('result.map',$result->id) }}"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="red" class="w-6 h-6">
                                                 <path fill-rule="evenodd" d="m7.539 14.841.003.003.002.002a.755.755 0 0 0 .912 0l.002-.002.003-.003.012-.009a5.57 5.57 0 0 0 .19-.153 15.588 15.588 0 0 0 2.046-2.082c1.101-1.362 2.291-3.342 2.291-5.597A5 5 0 0 0 3 7c0 2.255 1.19 4.235 2.292 5.597a15.591 15.591 0 0 0 2.046 2.082 8.916 8.916 0 0 0 .189.153l.012.01ZM8 8.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Z" clip-rule="evenodd" />
