f02cc8792d1daa169f2447b7ba8667dbb3438556
skybedy <skybedy@gmail.com>
2024-09-17 14:56:55 +0200

    Update css for mobile

--- resources/views/index/partials/event-list.blade.php
+++ resources/views/index/partials/event-list.blade.php
@@ -1,8 +1,8 @@
 
-<div class="sm:bg-white overflow-hidden shadow-sm sm:rounded-2xl mt-4">
-    <div class="py-4 md:px-2 flex flex-col sm:flex-row justify-around items-center">
+<div class="sm:bg-white overflow-hidden shadow-sm sm:rounded-2xl sm:mt-4">
+    <div class="py-4 md:px-2 flex flex-col sm:flex-row justify-around items-center mt-1 sm:mt-0">
         @foreach ($events as $event)
-            <div class="w-full sm:w-44 md:w-56 lg:w-[19rem] xl:w-96  sm:rounded-2xl  text-white text-center font-black bg-gray-600 mt-10 sm:mt-0 pt-4 pb-6">
+            <div class="w-full sm:w-44 md:w-56 lg:w-[19rem] xl:w-96  sm:rounded-2xl  text-white text-center font-black bg-gray-600 mt-8 sm:mt-0 pt-4 pb-6">
                 <div class="text-4xl sm:text-[3.4rem] md:text-7xl lg:text-8xl xl:text-9xl  sm:mt-7 xl:mt-3 border-b border-white mx-3 lg:mx-7 mb-1">{{ $event->name }}</div>
                 <div class="px-5 text-white text-center">
                     <a class="w-full inline-block rounded  bg-blue-400 hover:bg-blue-500 md:text-xl px-4 py-6 sm:py-2  mt-4 sm:mt-3" href="{{ route('event.result.index',$event->id) }}">Výsledky</a>
