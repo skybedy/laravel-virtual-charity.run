fff87e3adde53e62133fc8e1f455b362436bb130
skybedy <skybedy@gmail.com>
2024-09-18 10:02:28 +0200

    upravy stredniho boxu

--- resources/views/index/partials/event-list.blade.php
+++ resources/views/index/partials/event-list.blade.php
@@ -1,9 +1,16 @@
 
-<div class="sm:bg-white overflow-hidden shadow-sm sm:rounded-2xl sm:mt-4">
-    <div class="py-4 md:px-2 flex flex-col sm:flex-row justify-around items-center mt-1 sm:mt-0">
+
+
+<div class="bg-white overflow-hidden shadow-sm rounded-2xl mt-4 p-2 sm:p-4 md:px-2 flex flex-col sm:flex-row justify-around items-center">
+
+
+
         @foreach ($events as $event)
-            <div class="w-full sm:w-44 md:w-56 lg:w-[19rem] xl:w-96  sm:rounded-2xl  text-white text-center font-black bg-gray-600 mt-8 sm:mt-0 pt-4 pb-6">
+
+            <div class="w-full sm:w-44 md:w-56 lg:w-[19rem] xl:w-96  rounded-2xl  text-white text-center font-black bg-gray-600  sm:mt-0 pt-4 pb-6">
+
                 <div class="text-4xl sm:text-[3.4rem] md:text-7xl lg:text-8xl xl:text-9xl  sm:mt-7 xl:mt-3 border-b border-white mx-3 lg:mx-7 mb-1">{{ $event->name }}</div>
+
                 <div class="px-5 text-white text-center">
                     <a class="w-full inline-block rounded  bg-blue-400 hover:bg-blue-500 md:text-xl px-4 py-6 sm:py-2  mt-4 sm:mt-3" href="{{ route('event.result.index',$event->id) }}">Výsledky</a>
                     <a class="w-full inline-block rounded  bg-yellow-400 hover:bg-yellow-500 md:text-xl px-4 py-6 sm:py-2 mt-4 sm:mt-3" href="{{ route('event.startlist.index',$event->id) }}">Startovka</a>
@@ -13,7 +20,11 @@
                         <a class="w-full inline-block rounded  bg-green-400 hover:bg-gray-500 md:text-xl px-4 py-6 sm:py-2 mt-4 sm:mt-3" href="{{ route('event.show',$event->id) }}">Administrace</a>
                     @endif
                 </div>
+
             </div>
+
         @endforeach
-    </div>
+
+
+
 </div>
