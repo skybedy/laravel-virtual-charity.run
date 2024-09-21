58143008d48816fa0445d0ba57c3c6b2dcd439d7
skybedy <skybedy@gmail.com>
2024-09-21 09:51:08 +0000

    Update Events method

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -27,7 +27,7 @@ class Event extends Model
                 $join->on('events.id', '=', 'registrations.event_id')
                     ->where('registrations.user_id', '=', $userId);
             })
-            ->where(['events.platform_id' =>  env('PLATFORM_ID'),'events.serie_id' => env('ACTIVE_SERIE_ID')])
+            ->where(['events.platform_id' =>  env('PLATFORM_ID'),'display' => true])
             ->selectRaw('CASE WHEN registrations.user_id IS NULL THEN null ELSE 1 END as registration_status')
             ->orderBy('events.id', 'asc')
             ->get();
