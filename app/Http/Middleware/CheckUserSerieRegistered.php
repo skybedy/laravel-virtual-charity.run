2d961fd03d1dc3ab66e484c267c9c229da9d238b
skybedy <skybedy@gmail.com>
2024-09-24 20:18:00 +0200

    fix middleware

--- app/Http/Middleware/CheckUserSerieRegistered.php
+++ app/Http/Middleware/CheckUserSerieRegistered.php
@@ -6,34 +6,25 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Facades\DB;
+use App\Models\Registration;
 
 class CheckUserSerieRegistered
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
     public function handle(Request $request, Closure $next): Response
     {
+        $user_id = $request->user()->id;
 
-        $userId = $request->user()->id;
-        $serieId = env('ACTIVE_SERIE_ID');
+        $event_id = $request->route('eventId');
 
-        $exists = DB::table('registrations as r')
-        ->join('events as e', 'r.event_id', '=', 'e.id')
-        ->where('e.platform_id', env('PLATFORM_ID'))
-        ->where('e.serie_id', env('ACTIVE_SERIE_ID'))
-        ->where('r.user_id', $request->user()->id)
-        ->exists();
+        $exists = Registration::where(['event_id' => $event_id, 'user_id' => $user_id])->exists();
 
         if($exists)
         {
             return $next($request);
         }
 
-        return redirect()->route('registration.create')->with('error', 'Je nutné nejdříve se k závodům registrovat a uhradit startovné.');
-}
+        return redirect()->route('registration.create',$event_id)->with('error', 'Je nutné nejdříve se k závodům registrovat a uhradit startovné.');
+    }
 
 
 
