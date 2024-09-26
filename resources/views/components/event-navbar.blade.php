8e5074902dc9131e347fe1e59aff40332ace4285
skybedy <skybedy@gmail.com>
2024-09-26 09:41:14 +0200

    260924-1

--- resources/views/components/event-navbar.blade.php
+++ resources/views/components/event-navbar.blade.php
@@ -1,5 +1,5 @@
 <div class="flex justify-center sm:justify-between">
-    <h2 class="mb-1 text-4xl font-extrabold leading-none tracking-tight text-orange-600 md:text-5xl lg:text-6xl dark:text-white pl-1">{{ $event->name }}</h2>
+    <h2 class="mb-1 text-4xl font-extrabold leading-none tracking-tight text-orange-500 md:text-5xl lg:text-6xl dark:text-white pl-1">{{ $event->name }}</h2>
 
 
     @if(auth()->check())
@@ -12,21 +12,21 @@
 
 
 
-<nav class="bg-white border-b border-t border-blue-400">
+<nav class="bg-gradient-to-b from-orange-400 to-orange-500 rounded-md text-white font-bold">
       <div class="max-w-7xl mx-auto">
             <div class="flex h-10  px-1 justify-center sm:justify-between">
                   <div class="flex">
                         <div class="space-x-2 sm:space-x-5  sm:-my-px flex justify-center">
-                            <x-nav-link :href="route('event.startlist.index',$event->id)" :active="request()->routeIs('event.startlist.index')" class="text-blue-600 text-xs sm:text-sm">
+                            <x-nav-link :href="route('event.startlist.index',$event->id)" :active="request()->routeIs('event.startlist.index')" class="text-white text-xs sm:text-base">
                                 {{ __('Startovka') }}
                             </x-nav-link>
-                            <x-nav-link :href="route('event.result.index',$event->id)" :active="request()->routeIs('event.result.index')" class="text-blue-600 text-xs sm:text-sm">
+                            <x-nav-link :href="route('event.result.index',$event->id)" :active="request()->routeIs('event.result.index')" class="text-white text-xs sm:text-base">
                                 {{ __('Výsledky') }}
                             </x-nav-link>
-                            <x-nav-link :href="route('event.upload-url.create',$event->id)" :active="request()->routeIs('event.upload-url.create')" class="text-blue-600 text-xs sm:text-sm">
+                            <x-nav-link :href="route('event.upload-url.create',$event->id)" :active="request()->routeIs('event.upload-url.create')" class="text-white text-xs sm:text-base">
                                 {{ __('Nahrát odkaz ze Stravy') }}
                             </x-nav-link>
-                            <x-nav-link :href="route('event.upload-file.create',$event->id)" :active="request()->routeIs('event.upload-file.index')" class="text-blue-600 text-xs sm:text-sm">
+                            <x-nav-link :href="route('event.upload-file.create',$event->id)" :active="request()->routeIs('event.upload-file.index')" class="text-white text-xs sm:text-base">
                                 {{ __('Nahrát GPX soubor') }}
                             </x-nav-link>
 
--- resources/views/events/start-list.blade.php
+++ resources/views/events/start-list.blade.php
@@ -2,25 +2,27 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 bg-rd-500">
-                    <x-event-navbar :event="$event" />
-                    <table class="border-collapse w-full mt-5 text-xs sm:text-base">
-                        <tr>
-                            <th class="border-none px-2">Stč</th>
-                            <th class="border-none text-left px-2">Jméno</th>
-                            <th class="border-none text-left px-2">Tým/Město/Obec</th>
-                            <th class="border-none">Kategorie</th>
-                        </tr>
-                        @foreach ($startlists as $startlist )
-                            <tr class="odd:bg-gray-100 even:bg-white">
-                                <td class="border px-2 py-1 text-center">{{ $startlist->ids }}</td>
-                                <td class="border px-2 py-1">{{ $startlist->lastname }} {{ $startlist->firstname }}</td>
-                                <td class="border px-2 py-1">{{ $startlist->team }}</td>
-                                <td class="border text-center">{{ $startlist->name }}</td>
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-2xl">
+                <div class=" text-gray-900 bg-rd-500">
+                    <div class="homepage-box">
+                        <x-event-navbar :event="$event" />
+                        <table class="border-collapse w-full mt-5 text-xs sm:text-base">
+                            <tr>
+                                <th class="border-none px-2">Stč</th>
+                                <th class="border-none text-left px-2">Jméno</th>
+                                <th class="border-none text-left px-2">Tým/Město/Obec</th>
+                                <th class="border-none">Kategorie</th>
                             </tr>
-                        @endforeach
-                    </table>
+                            @foreach ($startlists as $startlist )
+                                <tr class="odd:bg-gray-100 even:bg-white">
+                                    <td class="border px-2 py-1 text-center">{{ $startlist->ids }}</td>
+                                    <td class="border px-2 py-1">{{ $startlist->lastname }} {{ $startlist->firstname }}</td>
+                                    <td class="border px-2 py-1">{{ $startlist->team }}</td>
+                                    <td class="border text-center">{{ $startlist->name }}</td>
+                                </tr>
+                            @endforeach
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
